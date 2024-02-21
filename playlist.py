5afda4dcfc73842edfd222c4e4b59d5e88799926
MadhanM123 <madhangopalvel@gmail.com>
2024-02-20 18:38:12 -0800

    playlist done

---
+++ playlist.py
@@ -0,0 +1,25 @@
+
+class Playlist:
+    def __init__(self, data):
+        self.data = data
+        self.songs = []
+
+    @property
+    def name(self):
+        return self.data["name"]
+    
+    @property
+    def id(self):
+        return self.data["id"]
+    
+    @property
+    def track_cnt(self):
+        return self.data["tracks"]["total"]
+    
+    @property
+    def url(self):
+        return self.data["external_urls"]["spotify"]
+    
+    @property
+    def is_public(self):
+        return self.data["public"]
